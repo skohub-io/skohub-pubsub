21d9feebda0b1c0db3ffd6b6ec259b3049e9125e
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-03-14 12:10:28 +0100

    List inbox contents for target
    If we stick with our approach that notifications should be transient, the
    inboxes will always be empty and notifications will only be distributed using
    the subscribe mechanism.
    
    See https://www.w3.org/TR/ldn/#receiving-inbox-contents

--- src/pubsub.js
+++ src/pubsub.js
@@ -3,7 +3,14 @@ import express from 'express'
 const pubsub = express()
 
 pubsub.get('/inbox', (req, res) => {
-  req.query.target ? res.status(200).send() : res.status(400).send()
+  if (!req.query.target) {
+    return res.status(400).send()
+  }
+  res.status(200).send({
+    '@context': 'http://www.w3.org/ns/ldp',
+    '@id': `${req.protocol}://${req.get('host')}${req.url}`,
+    'contains': []
+  })
 })
 
 export default pubsub
--- src/pubsub.test.js
+++ src/pubsub.test.js
@@ -4,14 +4,13 @@ import pubsub from './pubsub'
 describe('Test LDN inboxes', () => {
   test('requires target', async () => {
     const response = await request(pubsub).get('/inbox')
-    console.log('CODE', response.statusCode)
     expect(response.statusCode).toBe(400)
   })
 
   test('has an inbox for a target', async () => {
-    const response = await request(pubsub).get('/inbox').query({
-      target: 'https://lobid.org/gnd/118696432'
-    })
+    const response = await request(pubsub).get('/inbox')
+      .query({ target: 'https://lobid.org/gnd/118696432' })
     expect(response.statusCode).toBe(200)
+    expect(Object.keys(response.body).length).toBeGreaterThan(0)
   })
 })
