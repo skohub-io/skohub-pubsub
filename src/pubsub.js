025b3292000ee931254db01c7bf7409178669b94
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-03-14 16:45:16 +0100

    Receive and verify subscribe/unsubscribe requests
    See https://www.w3.org/TR/websub/#subscribing-and-unsubscribing

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import request from 'superagent'
+import crypto from 'crypto'
+
+const DEFAULT_LEASE_SECONDS = 7
 
 const pubsub = express()
+
+pubsub.use(express.urlencoded({ extended: true }))
 
 pubsub.get('/inbox', (req, res) => {
   if (!req.query.target) {
@@ -18,6 +24,31 @@ pubsub.post('/inbox', (req, res) => {
     return res.status(400).send()
   }
   res.status(202).send()
+})
+
+pubsub.post('/hub', async (req, res) => {
+  if (
+    req.headers['content-type'] !== 'application/x-www-form-urlencoded' ||
+    !req.body['hub.callback'] ||
+    !req.body['hub.mode'] ||
+    !req.body['hub.topic']
+  ) {
+    return res.status(400).send()
+  }
+  res.status(202).send()
+
+  const challenge = crypto.randomBytes(64).toString('hex')
+
+  try {
+    await request.get(req.body['hub.callback'])
+      .query(`hub.mode=${req.body['hub.mode']}`)
+      .query(`hub.topic=${req.body['hub.topic']}`)
+      .query(`hub.challenge=${challenge}`)
+      .query(`hub.lease_seconds=${req.body['hub.lease_seconds'] || DEFAULT_LEASE_SECONDS}`)
+    // TODO: save subscription
+  } catch (e) {
+    // TODO: cancel subscription request
+  }
 })
 
 export default pubsub
--- src/pubsub.test.js
+++ src/pubsub.test.js
@@ -1,4 +1,6 @@
 import request from 'supertest'
+import http from 'http'
+import querystring from 'querystring'
 import pubsub from './pubsub'
 
 describe('Test LDN inboxes', () => {
@@ -20,5 +22,35 @@ describe('Test LDN inboxes', () => {
       .set('Content-Type', 'application/ld+json')
       .send({ foo: 'bar' })
     expect(response.statusCode).toBe(202)
+  })
+})
+
+describe('Test WebSub subscriptions', () => {
+  test('accepts subscription request for a topic', async () => {
+    const parameters = Object.entries({
+      'hub.callback': `/foo`,
+      'hub.mode': 'subscribe',
+      'hub.topic': 'https://lobid.org/gnd/118696432'
+    }).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&')
+    const subscriptionRespone = await request(pubsub).post('/hub').send(parameters)
+    expect(subscriptionRespone.statusCode).toBe(202)
+  })
+
+  test('requests callback URL to verify subscription', async (done) => {
+    const callbackServer = http.createServer((req, res) => {
+      const [ path, query ] = req.url.split('?')
+      const challenge = querystring.parse(query)['hub.challenge']
+      expect(path).toBe('/callback')
+      expect(challenge).toBeDefined()
+      res.end(challenge, 'utf-8')
+      callbackServer.close(() => done())
+    }).listen(0)
+
+    const parameters = Object.entries({
+      'hub.callback': `http://localhost:${callbackServer.address().port}/callback`,
+      'hub.mode': 'subscribe',
+      'hub.topic': 'https://lobid.org/gnd/118696432'
+    }).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&')
+    await request(pubsub).post('/hub').send(parameters)
   })
 })
