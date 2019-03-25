884bc9b9c576f22bf09c5f239048cd0a469dfc30
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-03-25 18:59:00 +0100

    Factor out request validations

--- src/pubsub.js
+++ src/pubsub.js
@@ -5,16 +5,33 @@ import parseLinkHeader from 'parse-link-header'
 
 const DEFAULT_LEASE = 7
 const LDP_INBOX = 'http://www.w3.org/ns/ldp#inbox'
-const subscriptions = {}
+const webSubSubscriptions = {}
 
 const pubsub = express()
 
 pubsub.use(express.json({ type: ['application/ld+json', 'application/json'] }))
 pubsub.use(express.urlencoded({ extended: true }))
 
-pubsub.get('/inbox', (req, res) => {
-  if (!req.query.target) {
-    return res.status(400).send()
+const validateTarget = async target => {
+  if (!target) {
+    throw new Error('Target required')
+  }
+  const linkHeaderResponse = await request.get(target)
+  const linkHeader = parseLinkHeader(linkHeaderResponse.headers.link)
+  if (
+    !linkHeader ||
+    !linkHeader[LDP_INBOX] ||
+    linkHeader[LDP_INBOX].url !== `http://localhost:3000/inbox?target=${target}`
+  ) {
+    throw new Error('Invalid link headers for target URL')
+  }
+}
+
+pubsub.get('/inbox', async (req, res) => {
+  try {
+    await validateTarget(req.query.target)
+  } catch (e) {
+    return res.status(400).send(e.message)
   }
   res.status(200).send({
     '@context': 'http://www.w3.org/ns/ldp',
@@ -25,27 +42,51 @@ pubsub.get('/inbox', (req, res) => {
 
 pubsub.post('/inbox', async (req, res) => {
   const target = req.query.target
-  if (!target || req.headers['content-type'] !== 'application/ld+json') {
-    return res.status(400).send()
-  }
   try {
-    const linkHeadersResponse = await request.get(target)
-    const linkHeader = parseLinkHeader(linkHeadersResponse.headers.link)
-    if (
-      !linkHeader ||
-      !linkHeader[LDP_INBOX] ||
-      linkHeader[LDP_INBOX].url !== `http://localhost:3000/inbox?target=${target}`
-    ) {
-      throw new Error('Invalid target URL')
+    if (req.headers['content-type'] !== 'application/ld+json') {
+      throw new Error('Invalid Content-Type')
     }
+    await validateTarget(target)
   } catch (e) {
-    return res.status(400).send()
+    return res.status(400).send(e.message)
   }
+
   res.status(202).send()
-  Object.keys(subscriptions[target] || {}).forEach(callback => {
+
+  Object.keys(webSubSubscriptions[target] || {}).forEach(callback => {
     request.post(callback).send(req.body).then(res => res, err => err)
   })
 })
+
+const validateRequest = async (callback, mode, topic, lease = DEFAULT_LEASE) => {
+  if (!callback || !/subscribe|unsubscribe/.test(mode) || !topic) {
+    throw new Error('Invalid request')
+  }
+  const linkHeaderResponse = await request.get(topic)
+  const linkHeader = parseLinkHeader(linkHeaderResponse.headers.link)
+  if (
+    !linkHeader ||
+    !linkHeader.hub ||
+    !linkHeader.self ||
+    linkHeader.hub.url !== 'http://localhost:3000/hub' ||
+    linkHeader.self.url !== topic
+  ) {
+    throw new Error('Invalid topic or hub URL')
+  }
+}
+
+const verifyRequest = async (callback, mode, topic, lease = DEFAULT_LEASE) => {
+  const challenge = crypto.randomBytes(64).toString('hex')
+  const callbackResponse = await request.get(callback)
+    .query(`hub.mode=${mode}`)
+    .query(`hub.topic=${topic}`)
+    .query(`hub.challenge=${challenge}`)
+    .query(`hub.lease_seconds=${lease}`)
+
+  if (callbackResponse.text !== challenge) {
+    throw new Error('Invalid challenge in response')
+  }
+}
 
 pubsub.post('/hub', async (req, res) => {
   if (req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
@@ -56,52 +97,28 @@ pubsub.post('/hub', async (req, res) => {
     'hub.callback': callback,
     'hub.mode': mode,
     'hub.topic': topic,
-    'hub.lease_seconds': lease = DEFAULT_LEASE
+    'hub.lease_seconds': lease
   } = req.body
 
-  if (!callback || !/subscribe|unsubscribe/.test(mode) || !topic) {
-    return res.status(400).send()
-  }
-
   try {
-    const linkHeadersResponse = await request.get(topic)
-    const linkHeader = parseLinkHeader(linkHeadersResponse.headers.link)
-    if (
-      !linkHeader ||
-      !linkHeader.hub ||
-      !linkHeader.self ||
-      linkHeader.hub.url !== 'http://localhost:3000/hub' ||
-      linkHeader.self.url !== topic
-    ) {
-      throw new Error('Invalid topic or hub URL')
-    }
+    await validateRequest(callback, mode, topic, lease)
   } catch (e) {
-    return res.status(400).send()
+    return res.status(400).send(e.message)
   }
 
   res.status(202).send()
 
-  const challenge = crypto.randomBytes(64).toString('hex')
-
   try {
-    const callbackResponse = await request.get(callback)
-      .query(`hub.mode=${mode}`)
-      .query(`hub.topic=${topic}`)
-      .query(`hub.challenge=${challenge}`)
-      .query(`hub.lease_seconds=${lease}`)
-
-    if (callbackResponse.text !== challenge) {
-      throw new Error('Invalid challenge in response')
-    }
-
-    if (mode === 'subscribe') {
-      subscriptions[topic] = subscriptions[topic] || {}
-      subscriptions[topic][callback] = lease
-    } else {
-      delete subscriptions[topic][callback]
-    }
+    await verifyRequest(callback, mode, topic, lease)
   } catch (e) {
-    // discard subscription request
+    return // discard subscription request
+  }
+
+  if (mode === 'subscribe') {
+    webSubSubscriptions[topic] = webSubSubscriptions[topic] || {}
+    webSubSubscriptions[topic][callback] = lease
+  } else {
+    delete webSubSubscriptions[topic][callback]
   }
 })
 
