6be4de4b417ba8bd93811ef94d052f3f74a399eb
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-11-06 17:18:16 +0100

    Add inbox endpoint confirming Follow actions

--- src/activitypub.js
+++ src/activitypub.js
@@ -4,13 +4,20 @@ import express from 'express'
 import uuid from 'uuid'
 import url from 'url'
 import morgan from 'morgan'
+import request from 'superagent'
+import crypto from 'crypto'
+const { URL } = require('url')
 
 const INBOX = []
 const FOLLOWERS = {}
+const POST_HEADERS = {
+  'Content-Type': 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+}
+const GET_HEADERS = {
+  'Accept': 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+}
 const PRIV_KEY = fs.readFileSync(path.resolve('data', 'private.pem'), 'utf8')
 const PUB_KEY = fs.readFileSync(path.resolve('data', 'public.pem'), 'utf8')
-
-console.log(PRIV_KEY, PUB_KEY)
 
 const app = express()
 
@@ -72,6 +79,45 @@ app.get('/u/:id', (req, res) => {
     }
   }
   res.send(actor)
+})
+
+app.post('/inbox', async (req, res) => {
+  const action = req.body
+
+  if (action.type !== 'Follow') {
+    console.warn('Unhandled action type', action.type)
+    res.send()
+    return
+  }
+
+  // send signed accept message to inbox of action.actor
+  const { body: actor } = await request.get(action.actor).set(GET_HEADERS)
+  const accept = {
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    'actor': action.object,
+    'id': `${req.publicHost}/${uuid.v4()}`,
+    'object': action,
+    'type': 'Accept'
+  }
+  const date = (new Date()).toUTCString()
+  const { pathname, hostname } = new URL(actor.inbox)
+  const signer = crypto.createSign('SHA256')
+  signer.update(`(request-target): post ${pathname}\nhost: ${hostname}\ndate: ${date}`)
+  signer.end()
+  const signature = signer.sign(PRIV_KEY).toString('base64')
+  const header = `keyId="${action.object}",headers="(request-target) host date",signature="${signature}"`
+  try {
+    const resp = await request.post(actor.inbox).send(accept).set(POST_HEADERS).set({
+      'Host': hostname,
+      'Date': date,
+      'Signature': header
+    })
+    console.log('SUCCESS', resp.status)
+    res.status(201).send()
+  } catch (e) {
+    console.error('ERROR', e)
+    res.status(500).send()
+  }
 })
 
 app.listen(3000, function() {
