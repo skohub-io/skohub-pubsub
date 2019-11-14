2cf736b6497c673d35c12c50e8d68f73edfc0483
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-11-14 12:23:23 +0100

    Read server port from env

--- src/activitypub.js
+++ src/activitypub.js
@@ -257,6 +257,6 @@ app.post('/inbox', async (req, res) => {
   res.status(201).send()
 })
 
-app.listen(3000, function() {
-  console.log('Inbox listening on port 3000!')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Inbox listening on port ${process.env.PORT || 3000}!`)
 })
