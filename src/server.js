87ade3917ec9e802bc9ad9a03b0e284f47da0c2d
Felix Ostrowski <felix.ostrowski@googlemail.com>
2020-01-09 16:25:59 +0100

    Use mongoDB persistence

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import activitypub from './activitypub'
-import filesystem from './filesystem'
+import mongodb from '../src/mongodb'
 
-activitypub(filesystem('data')).listen(process.env.PORT || 3000, () => {
-  console.log(`Inbox listening on port ${process.env.PORT || 3000}!`)
-})
+const { MONGO_HOST, MONGO_PORT, MONGO_DB, PORT } = process.env
+
+mongodb({
+  host: MONGO_HOST || 'localhost',
+  port: MONGO_PORT || 27017,
+  db: MONGO_DB || 'skohub'
+}).then(db => activitypub(db).listen(PORT || 3000,
+  () => console.log(`Inbox listening on port ${PORT || 3000}!`)))
