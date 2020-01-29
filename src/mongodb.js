3f53759b9071227684a59e3cb0a645657470256b
Felix Ostrowski <felix.ostrowski@googlemail.com>
2020-01-29 12:40:54 +0100

    Destructure mongodb config object

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,23 +1,23 @@
 import { MongoClient, Server } from 'mongodb'
 
-const mongodb = async config => {
-  const client = new MongoClient(new Server(config.host, config.port))
+const mongodb = async ({ host, port, db }) => {
+  const client = new MongoClient(new Server(host, port))
   await client.connect()
-  await client.db(config.db).createIndex('followers', { object: 1 }, { unique: true })
-  await client.db(config.db).createIndex('messages', { id: 1 }, { unique: true })
+  await client.db(db).createIndex('followers', { object: 1 }, { unique: true })
+  await client.db(db).createIndex('messages', { id: 1 }, { unique: true })
 
   return {
-    getFollowers: async object => ((await client.db(config.db).collection('followers')
+    getFollowers: async object => ((await client.db(db).collection('followers')
       .findOne({ object })) || { followers: [] }).followers,
-    addFollower: async (object, actor) => client.db(config.db).collection('followers')
+    addFollower: async (object, actor) => client.db(db).collection('followers')
       .updateOne({ object }, { $setOnInsert: { object }, $addToSet: { followers: actor } }, { upsert: true }),
-    removeFollower: async (object, actor) => client.db(config.db).collection('followers')
+    removeFollower: async (object, actor) => client.db(db).collection('followers')
       .updateOne({ object }, { $pull: { followers: actor } }),
-    getMessagesFor: async actor => ((await client.db(config.db).collection('messages')
+    getMessagesFor: async actor => ((await client.db(db).collection('messages')
       .find({ actor, type: 'Create' }).toArray()) || []).map(message => message.object),
-    getMessage: async id => client.db(config.db).collection('messages')
+    getMessage: async id => client.db(db).collection('messages')
       .findOne({ id }),
-    saveMessage: async message => client.db(config.db).collection('messages')
+    saveMessage: async message => client.db(db).collection('messages')
       .replaceOne({ id: message.id }, message, { upsert: true })
   }
 }
